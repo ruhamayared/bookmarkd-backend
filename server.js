5793e5dfddef2e5a2de9e31d9308ad02fefcdb43
samwhitford11 <samantha.fichte98@gmail.com>
2023-01-14 15:31:35 -0700

    adds create route

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 //Import dependencies
-import express from "express"
-import morgan from "morgan"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
 //Get env variables
 dotenv.config()
 
 //Create express app
 const app = express()
+
 
 //Register middleware
 app.use(cors())
@@ -19,6 +20,31 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("The route works!")
 })
+
+
+// Create Route
+app.post('/bookmark', async (req, res) => {
+  try{
+    res.json(await Bookmark.create(req.body))
+  }catch(error){
+    res.status(400).json(error)
+  }
+});
+
+app.put ('/bookmark/:id', async (req, res) => {
+  try{
+    res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
+
+
+
+
+
+
+
 
 //Listener
 const PORT = process.env.PORT ?? 4000
